00e7d28cbf1580e9ed7d3f38a7bdc033cea64af6
Eyal Yosipovich <eyalyosi@gmail.com>
2022-03-22 20:42:34 +0200

    push

---
+++ src/components/gig-preview.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <p><img src="" alt="">
+        {{gig.title}}</p>
+    <p>{{gig.price}}</p>
+    <p>{{gig.fullname}}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "gig-preview",
+  props: {
+    gig: Object,
+  },
+  components: {
+    // customCard,
+  },
+  computed: {
+  },
+  methods: {
+  },
+  created() {
+  },
+};
+</script>
