4d5f0b2740e3d3cabdf4a1521db764835a9f6ff4
OphirNaor <ophir1991@gmail.com>
2022-03-23 23:51:20 +0200

    styling gig preview

--- src/components/gig-filter.vue
+++ src/components/gig-filter.vue
@@ -1,7 +1,6 @@
 <template>
-<<<<<<< HEAD
     <section class="gig-filter">
-        <input @change="setFilter" v-model="filterBy.category" type="text" placeholder="Try logo" />
+        <input @change="setFilter" v-model="filterBy.title" type="text" placeholder="Try logo" />
     </section>
 </template>
 <script>
@@ -9,7 +8,7 @@ export default {
     data() {
         return {
             filterBy: {
-                category: "",
+                title: "",
             },
         };
     },
@@ -17,29 +16,6 @@ export default {
         setFilter() {
             this.$emit("setFilter", JSON.parse(JSON.stringify(this.filterBy)));
         },
-=======
-  <section class="gig-filter">
-    <input
-      @change="setFilter"
-      v-model="filterBy.title"
-      type="text"
-      placeholder='Try "logo"'
-    />
-  </section>
-</template>
-<script>
-export default {
-  data() {
-    return {
-      filterBy: {
-        title: "",
-      },
-    };
-  },
-  methods: {
-    setFilter() {
-      this.$emit("setFilter", JSON.parse(JSON.stringify(this.filterBy)));
->>>>>>> b89a4cd615ca9a87276057d5d3a01d47a9d76b52
     },
-};
+}
 </script>
