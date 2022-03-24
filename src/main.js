fbde4a1474e8801f3d2885a6473826fa912d6206
Or Kopel <smilgarden@gmail.com>
2022-03-24 17:00:36 +0200

    17:00

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store/index.js'
 import './styles/styles.scss'
+import ElementPlus from 'element-plus'
+
+
 
 const app = createApp(App)
 
+
+app.use(ElementPlus)
 app.use(router)
 app.use(store)
 app.mount('#app')
