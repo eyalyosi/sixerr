95eb95b4a3784401483fadd4bcab9b437ff7ddea
Or Kopel <smilgarden@gmail.com>
2022-03-27 13:17:03 +0300

    gg

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <template>
-<<<<<<< HEAD
-  <!-- <section> -->
-
-    <!-- //OFIR, I COMMENT THE HEADER FUNCTION UP BECAUSE I COULDNT 
-//SEE WHAT IM RENDERING, OR -->
-    <!-- <app-header class="app-header" :class="{ change_color: scrollPosition > 50 }" /> -->
-    <app-header class="app-header"/>
-    <div>
-=======
   <section class="main-section">
     <app-header :class="$route.meta.headerClass" />
 
     <div class="main-content-container">
->>>>>>> ab6c45e58c6e879049f1a041ca570be3d3f2d6ea
       <router-view />
     </div>
     <app-footer />
-  <!-- </section> -->
+  </section>
 </template>
 
 <script>
