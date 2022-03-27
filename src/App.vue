ab6c45e58c6e879049f1a041ca570be3d3f2d6ea
OphirNaor <ophir1991@gmail.com>
2022-03-27 12:59:07 +0300

    improving layout style

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
-<<<<<<< HEAD
   <section class="main-section">
-=======
-<<<<<<< HEAD
-    <!-- //OFIR, I COMMENT THE HEADER FUNCTION UP BECAUSE I COULDNT 
-    //SEE WHAT IM RENDERING, OR-->
-    <!-- <app-header class="app-header" :class="{ change_color: scrollPosition > 50 }" /> -->
-    <app-header class="app-header" />
-    <router-view />
-=======
-  <section>
->>>>>>> 9edd6e05b92a7c0d0d4419c5ea5dd89487745c52
     <app-header :class="$route.meta.headerClass" />
 
     <div class="main-content-container">
       <router-view />
     </div>
->>>>>>> 060c98fc050c8d410c81626aaf116614a6458750
     <app-footer />
+  </section>
 </template>
 
 <script>
